9e29c1e92296ba2c1edeaa13bd167d06ab27b2db
jaydenrobinette416 <jaydenrobinette416@gmail.com>
2026-05-31 21:07:29 -0400

    Update connectDB to use MONGODB_URI
    Refactor connectDB function to use environment variable for MongoDB URI.

--- api/db.js
+++ api/db.js
@@ -1,3 +1,18 @@
-export default function connectDB() {
-  return "mongodb+srv://<apparatusadmin>:<Base98AD>@apparatuscheck.4u79vqc.mongodb.net/?appName=apparatuscheck";
+import { MongoClient } from "mongodb";
+
+let client;
+let db;
+
+export async function connectDB() {
+  if (db) return db;
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("Missing MONGODB_URI environment variable");
+  }
+
+  client = new MongoClient(process.env.MONGODB_URI);
+  await client.connect();
+
+  db = client.db("apparatus_system");
+  return db;
 }
